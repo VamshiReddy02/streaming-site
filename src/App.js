ff48e71f37d17cae9af1a1e7e53940c09fdea9b6
Vamshi Reddy <vamshiproject02@gmail.com>
2023-09-24 15:58:21 +0530

    navbar work is completed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import './App.css';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Box sx={{ background: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:serachTerm" element={<SearchFeed />} />
+        </Routes>
+
+    </Box>
+    </BrowserRouter>
+  );
+}
+
+export default App;
