3ee7d1d6096cefc73764c5061c09a947f50fd863
Vamshi Reddy <vamshiproject02@gmail.com>
2023-09-24 17:41:35 +0530

    sidebar work is completed

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,36 @@
+import { Stack } from '@mui/material'
+import { categories } from '../utils/constants'
+import React from 'react'
+import { Opacity } from '@mui/icons-material'
+
+const selectedCategory = 'Coding'
+
+const SideBar = () => {
+  return (
+    <Stack
+        direction="column"
+        sx={{
+            overflow: "auto",
+            height: {sx: 'auto', md: '95%'},
+            FlexDirection: { md: 'row'},
+        }}>
+            {categories.map((category) => (
+                <button 
+                    className='category-btn'
+                    style={{
+                        background: category.name === 
+                        selectedCategory && '#FC1503', 
+                        color: 'white'
+                    }}
+                    key={category.name}
+                    >
+                    <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+                    <span style={{ Opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+                </button>
+            ))}
+
+    </Stack>
+  )
+}
+
+export default SideBar
